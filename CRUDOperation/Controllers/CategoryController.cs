9a06d2329cf663a91452fddb525fa795b2703c32
Amit7888 <amitshinde4464@gmail.com>
2023-03-25 14:17:22 +0530

    Add project files.

---
+++ CRUDOperation/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDOperation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CRUDDBContexts contexts = new CRUDDBContexts();
+        public ActionResult Index()
+        {
+            var listofData1 = contexts.Categories.ToList();
+            return View(listofData1);
+           
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            contexts.Categories.Add(model);
+            contexts.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = contexts.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+
+            return View(data);
+        }
+        [HttpPost]
+
+        public ActionResult Detail(int id)
+        {
+            var data = contexts.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult Edit(Category Model)
+        {
+            var data = contexts.Categories.Where(x => x.CategoryId == Model.CategoryId).FirstOrDefault();
+            if (data != null)
+            {
+                data.CategoryName = Model.CategoryName;
+                contexts.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        public ActionResult Delete(int id)
+        {
+            var data = contexts.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            contexts.Categories.Remove(data);
+            contexts.SaveChanges();
+            ViewBag.Message = "Record Delete Sucessfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CRUDOperation/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDOperation.Controllers
+{
+    public class HomeController : Controller
+    {
+        CRUDDBContext _context = new CRUDDBContext();
+        public ActionResult Index()
+        {
+            var listofData = _context.Products.ToList();
+            return View(listofData);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product model)
+        {
+            _context.Products.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Insert Successfully";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+
+        public ActionResult Detail(int id)
+        {
+            var data = _context.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            return View(data);
+        }
+        public ActionResult Edit(Product Model)
+        {
+            var data = _context.Products.Where(x => x.ProductId == Model.ProductId).FirstOrDefault();
+            if (data != null)
+            {
+                data.ProductName = Model.ProductName;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+       
+        
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Products.Where(x => x.ProductId == id).FirstOrDefault();
+            _context.Products.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Message = "Record Delete Sucessfully";
+            return RedirectToAction("Index");
+        }
+       
+    }
+}
